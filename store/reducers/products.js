0706ce6b751e3af240caff7a8bece5f754d086d1
Siddhi Shah <58992453+siddhishah222@users.noreply.github.com>
2022-02-17 16:54:03 -0500

    Add files via upload

---
+++ store/reducers/products.js
@@ -0,0 +1,73 @@
+import PRODUCTS from '../../data/dummy-data';
+import {DELETE_PRODUCT} from '../actions/products';
+import {UPDATE_PRODUCT} from '../actions/products';
+import {CREATE_PRODUCT} from '../actions/products';
+import { SET_PRODUCTS } from '../actions/products';
+import Product from '../../models/product';
+
+const initialState = {
+  availableProducts: [],
+  userProducts: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return {
+        availableProducts: action.products,
+        userProducts: action.userProducts,
+      }
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        // new Date().toString(),   dummy id
+        action.productData.id,
+        action.productData.ownerId,
+        action.productData.title,
+        action.productData.description,
+        action.productData.imageUrl,
+        action.productData.price,
+      );
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct),
+      };
+
+    case UPDATE_PRODUCT:
+      const productIndex = state.userProducts.findIndex(
+        prod => prod.id === action.pid,
+      );
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[productIndex].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIndex].price,
+      );
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIndex] = updatedProduct;
+      const availableProductsIndex = state.availableProducts.findIndex(
+        prod => prod.id === action.pid,
+      );
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedAvailableProducts[availableProductsIndex] = updatedProduct;
+      return {
+        ...state,
+        availableProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts,
+      };
+
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter(
+          product => product.id !== action.pid,
+        ),
+        availableProducts: state.availableProducts.filter(
+          product => product.id !== action.pid,
+        ),
+      };
+  }
+  return state;
+};
